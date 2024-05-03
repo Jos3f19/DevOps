50aa2c6dc72a3f169d9cfb73d3f7aa34bbd3ed0d
Josef Bohórquez <49245883+Jos3f19@users.noreply.github.com>
2024-05-02 20:24:07 -0500

    API was cleaned

---
+++ models/Materias.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const materiasSchema = new mongoose.Schema({
+  codigoMateria: String,
+  Nombre: String,
+  services: {
+    type: Map,
+    of: String
+  }
+})
+module.exports = mongoose.model('Materias', materiasSchema)
