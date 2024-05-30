233ff74929f29254951f1d49e9c5f716da275b51
Josef Bohórquez <49245883+Jos3f19@users.noreply.github.com>
2024-05-30 17:10:45 -0500

    Port change
    The port was changed to 8080 to verify if cloud run works

--- main.tf
+++ main.tf
@@ -14,3 +14,45 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+# Enables the Cloud Run API
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+
+  disable_on_destroy = true
+}
+
+# Allow unauthenticated users to invoke the service
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_service.run_service.name
+  location = google_cloud_run_service.run_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+# Create the Cloud Run service
+resource "google_cloud_run_service" "run_service" {
+  name = "newdevops"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/responsive-hall-419216/devopsudem@sha256:579a0b4f222db95d9340fc5a9deef1eccaf9c4cbcdfc93f11d1da48ed94ed993"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  # Waits for the Cloud Run API to be enabled
+  depends_on = [google_project_service.run_api]
+}
+
+# Display the service URL
+output "service_url" {
+  value = google_cloud_run_service.run_service.status[0].url
+}
